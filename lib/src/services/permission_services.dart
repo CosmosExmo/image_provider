a444e227686e0c40c51bd7d2acf70b7144aeae01
Veli Kaan Çetinel <32401058+trailon@users.noreply.github.com>
2023-10-20 15:01:52 +0300

    update try storage access for new api versions of android

--- lib/src/services/permission_services.dart
+++ lib/src/services/permission_services.dart
@@ -1,17 +1,17 @@
-
 import 'package:permission_asker/permission_asker.dart';
 
 class PermissionServices {
   Future<bool> getStorageRequest() async {
-    PermissionStatus status = PermissionStatus.denied;
+    final status1 = await Permission.storage.request();
+    final status2 = await Permission.photos.request();
+    final status3 = await Permission.manageExternalStorage.request();
+    final isGranted = [
+      status1,
+      status2,
+      status3,
+    ].any((element) => element.isGranted);
 
-    PermissionAsker(onPermissionData: (value) {
-      status = value.status;
-    });
-
-    status = await Permission.storage.request();
-
-    return status.isGranted;
+    return isGranted;
   }
 
   Future<PermissionStatus> getCameraRequest() async {
